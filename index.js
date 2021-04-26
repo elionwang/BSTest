7e7a33fa557045a98183d9cc46871da469e336e2
Elion Wang <>
2021-04-26 19:41:58 +0000

    26-04-2021

--- index.js
+++ index.js
@@ -1,66 +1,30 @@
+const mySecret = process.env['TOKEN']
+const prefix = process.env['prefix']
 const Brawlstars = require("brawlstars.js")
 const Discord = require('discord.js')
-const DiscordClient = new Discord.Client()
-const command = require('./command')
-const token  = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImRmMWFkNTExLTZmOGMtNDI0NC04ODE2LTdjYjllZTI0ZGM0MCIsImlhdCI6MTYxODkyMTgxNywic3ViIjoiZGV2ZWxvcGVyL2ZhYjVhMzFlLWMzNzYtYzlhOS0xMWFkLTFlYjNhMDQ3MDEwNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMzQuNzIuMTAxLjkxIiwiMzQuNjYuODAuMTgyIl0sInR5cGUiOiJjbGllbnQifV19.rP_Afxpsp0UfQkWdgyJhWRgWa3hrhk5Cj7wLHkf6BD5fqsCX_vTEoUSn43-kWW89RBDYYLhwcxlKSGtwA22gWA"
+const client = new Discord.Client()
+const token  = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcxNTE4ZjFhLWI0NTQtNDVjMS04YzU3LTRhZjM0ODI0OTJjNyIsImlhdCI6MTYxOTQ2MjY0OCwic3ViIjoiZGV2ZWxvcGVyL2ZhYjVhMzFlLWMzNzYtYzlhOS0xMWFkLTFlYjNhMDQ3MDEwNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTA0LjE5Ny4xMjQuODQiLCIzNC43Mi4xMTQuMTQ4IiwiMTA0LjE1NS4xNDQuMjI4IiwiMzUuMjAyLjIzMS4xMSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.XYwmoyC3zTb2KIA5ZmY6sb5rTxhJwZbiX7s3s7k1DWXYsXM8kAG_EDia1q4saYf3YgLr4uJQCXFv_7n9wM4lRA"
+const bsclient = new Brawlstars.Client(token)
+require('dotenv').config()
 
-const client = new Brawlstars.Client(token)
-
-
-;(async function () {
-  const X = '###########################'
-  const player = await client.getPlayer('#PYV80U2J')
-  
-  console.log(X)
-  console.log(player.name)
-  console.log(player.highestTrophies)
-  console.log(player.brawlerCount)
-  console.log(player.getWins(Brawlstars.Constants.WIN_TYPE_TRIO))
-  console.log(player.getWins(Brawlstars.Constants.WIN_TYPE_TOTAL));
-  console.log(player.gadgetsCount) 
-  console.log(player.starpowersCount)
-
-  console.log('By Name:', player.getBrawlerByName('MAX'))
-  console.log('By ID:', player.getBrawlerById('16000020'))
-
-  console.log('Rumble', player.bestRoboRumbleTime) 
-  console.log('BigBrawler', player.bestTime(1))
-
-  if (player.club) {
-    const club = await client.getClub(player.club.tag)
-    console.log(X)
-    console.log(club.type)
-    console.log(club.getMemberRank(player.tag))
-    console.log(club.getMemberRole(player.tag))
-    console.log(club.isFull)
-  }
-  
-  const FrenchRank = await client.getRanking('FR', 'players')
-  console.log(X)
-  console.log(FrenchRank.getTop(3))
-  console.log(FrenchRank.isRanked(player.tag))
-  const brawlers = await client.getBrawlers()
-  console.log(X)
-  console.log(brawlers.count)
-  console.log(brawlers.getBrawlersNames())
-  console.log(brawlers.getBrawlerStarPowersByName('FRANK'))
-})()
-
-DiscordClient.on('ready', () => {
+client.on('ready', () => {
   console.log(`Client ready on ${client.user.tag}`)
-  
-  command(client, 'ping', (message) => {
-    message.reply('Ping aan het berekenen...').then(resultMessage => {
-      const ping = resultMessage.createdTimestamp - message.createdTimestamp
-      
-      resultMessage.edit(`Bot latency: \`${ping} ms\` API latency: \`${client.ws.ping} ms\``)
-    })
-  })
-  
-  command(client, 'name', (message) => {
-    
-    message.reply(player.name)
-  })
 })
 
-DiscordClient.login(process.env.TOKEN)
+client.on('message', async message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command === 'bs') {
+    const player = await bsclient.getPlayer((message.content.slice(6)));
+    const bsembed = new Discord.MessageEmbed().setTitle('Ben jij dit?').setColor(0x008080).addFields(
+		{ name: 'Player Name', value: player.name },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Trophies', value: `\`${player.trophies}\``, inline: true },
+		{ name: 'Club', value: player.club.name, inline: true },
+	);
+    message.channel.send(bsembed)
+  }
+})
+
+client.login(mySecret)
